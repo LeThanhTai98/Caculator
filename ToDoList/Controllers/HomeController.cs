6501fa3960b5ad3d33b2431806bfc458508db16b
Thanh Tai <Thanh Tai@DESKTOP-6H9QI3M>
2020-10-03 06:58:45 +0700

    ini project

---
+++ ToDoList/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var list = new ToDoListModel {
+                new ToDoListItem(){ id = 1 , content = "task 1" },
+                new ToDoListItem(){ id = 1 , content = "task 2"},
+                new ToDoListItem(){ id = 1 , content = "task 3"},
+                new ToDoListItem(){ id = 1 , content = "task 4"},
+                new ToDoListItem(){ id = 1 , content = "task 5"}
+            };
+            return View(list);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class ToDoListItem
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+    }
+    public class ToDoListModel : List<ToDoListItem>
+    {
+        
+    }
+}
